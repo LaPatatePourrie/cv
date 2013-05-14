2f40e520364a3427a5ed1004bbb8dd6d9a777ff0
etudiant <etudiant@webservice.(none)>
2013-05-14 16:55:37 +0200

    storage

--- Symfony/web/js/app/controllers/main.js
+++ Symfony/web/js/app/controllers/main.js
@@ -1,10 +1,5 @@
-'use strict';
-
 app.controller('MainCtrl', function MainCtrl( $scope, $location, storage, filterFilter ) {
-	var users = $scope.users = storage.get({
-		success: function (users) {
-			console.log(users);
-		},
-		error: function () {alert('ERROR')}
+	var users = $scope.users = storage.get('users', function (users) {
+		console.log(users);
 	});
 });
--- Symfony/web/js/app/services/storage.js
+++ Symfony/web/js/app/services/storage.js
@@ -1,17 +1,18 @@
 'use strict';
 
 app.factory( 'storage', function($http) {
-  var STORAGE_ID = 'storage-angularjs';
+	return {
+		get: function(query, callback) {
+			switch (query) {
 
-  return {
-	get: function(callbacks) {
-		$http({method: 'GET', url: 'http://localhost:1337/user/'})
-		.success(callbacks.success())
-		.error(callbacks.error())
-	},
-
-	put: function( users ) {
-		localStorage.setItem(STORAGE_ID, JSON.stringify(users));
-	}
-  };
+				case 'users':
+					callback([
+						{name:'Alexis'}, 
+						{name:'Jean'}
+					]);
+				break;
+				
+			}
+		}
+	};
 });
